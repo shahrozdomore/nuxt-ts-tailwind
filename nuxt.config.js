b495f4c544cb27e1dc722804b400d9b39a49a97e
Shaharoz Ali Khan <shahrozdomore@gmail.com>
2021-06-16 02:12:23 +0500

    initial commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,11 @@ export default {
     title: "nuxt-ts-tailwind",
     meta: [
       { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        name: "viewport",
+        content:
+          "width=device-width, initial-scale=1, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+      },
       { hid: "description", name: "description", content: "" }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
